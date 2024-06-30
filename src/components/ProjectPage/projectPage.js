d2becbbad9de33dfac7172dc81d61515c7ce7537
Dim-crea <hauterville.dimitry@gmail.com>
2024-06-30 23:45:15 +0200

    Page project in progress

---
+++ src/components/ProjectPage/projectPage.js
@@ -0,0 +1,17 @@
+export default function (){
+    return (
+        <>
+            <div className="">
+                <img></img>
+                <div>
+                    <h2></h2>
+                    <p></p>
+                    <div>
+                        <img></img>
+                        <img></img>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ src/layout/footerPortfolio.js
@@ -0,0 +1,13 @@
+
+export default function FooterPortfolio () {
+    return (
+        <footer className="border">
+            <div>
+            <p className="">Img</p>
+            </div>
+            {/* <img src="../assets/picture/GitHub.svg" alt="Logo GitHub"></img> */}
+            
+        </footer>
+        
+    )
+}
